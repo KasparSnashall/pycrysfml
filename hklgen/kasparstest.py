211448c87da2c59c6bc1ae6a144c981ec80dacf4
KasparSnashall <snashalk@tcd.ie>
2015-08-20 13:09:56 +0100

    removed some null code

--- hklgen/kasparstest.py
+++ hklgen/kasparstest.py
@@ -3,37 +3,36 @@ import numpy as np
 import fswig_hklgen as H
 np.seterr(divide="ignore", invalid="ignore")    
 
-DATAPATH = os.path.dirname(os.path.abspath(__file__))
-infoFile = os.path.join(DATAPATH,"test.cif")
-print "here"
-(spaceGroup, crystalCell, atoms) = H.readInfo(infoFile)
+DATAPATH = os.path.dirname(os.path.abspath(__file__)) # the path
+infoFile = os.path.join(DATAPATH,"kasparstest.cif")
 
+
+## returns the sin of the angle, this is copied from fswig_hklgen
 def getS(tt, wavelength):
     return np.sin(np.radians(tt/2))/wavelength
 
 def main():
-    print "here"
-    #spaceGroup = None
-    #cell = None
+    
     wavelength = 1.5403
     cell = H.CrystalCell([11.372,10.272,7.359],[108.92,71.07,96.16]) # AABHTZ cell parameters
-    uvw = [0.151066141044763,-0.0914698313404034,0.0693509296318546]
+    uvw = [0.151066141044763,-0.0914698313404034,0.0693509296318546] ## this is for a uvw transformation
+    
+    
     #H.diffPattern(infoFile=infoFile, wavelength=wavelength,
     #              cell=cell, uvw=uvw, scale=1.40313478468024,
     #              info=True,plot=True)
-    
+    # this created the diffraction pattern but relies of the type of atoms
     
     sMin, sMax = getS(0.0, wavelength), getS(180.0, wavelength)
     spaceGroup = None
     infofile = H.readInfo(infoFile)
     if (spaceGroup == None): spaceGroup = infofile[0]
     if (cell == None): cell = infofile[1]
-    refList = H.hklGen(spaceGroup, cell, sMin, sMax, True, xtal=False) # gets the reflections list but not the positions
+    refList = H.hklGen(spaceGroup, cell, sMin, sMax, True, xtal=False) # generates list of hkl [1,1,1]
     for value in refList:
         print value.hkl
     
     
 
 if __name__ == "__main__":
-    # program run normally
     main()
